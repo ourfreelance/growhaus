6e435f2d9d8aaddbf468ce79bf9320893aca11de
Huda Damar <60374935+hudadamar21@users.noreply.github.com>
2022-07-29 14:29:38 +0700

    email & url validation, complete all

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,8 +1,26 @@
 import "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"
 
+const formInputs = document.querySelectorAll('.form-input')
+
+const navigationForm = document.querySelectorAll('.swiper-navigation-form')
+const slideNextButtons = document.querySelectorAll('.swiper-navigation-form.swiper-next-slide')
+
+const paginationForm = document.querySelector('.swiper-pagination-form')
+
+const thanksForSubmit = document.querySelector('#thanks-for-submit')
+const formSubmitButton = document.querySelector('.form-submit-button')
+
+const toggleMenu = document.querySelector('.toggle-menu')
+const menuIcon = toggleMenu.querySelector('img')
+const menu = document.querySelector('.menu')
+
+
 const swiper = new Swiper('.swiper-form', {
   allowTouchMove: false,
   slidesPerView: 1,
+  keyboard: {
+    enabled: true
+  },
   navigation: {
     prevEl: '.swiper-prev-slide',
     nextEl: '.swiper-next-slide',
@@ -25,17 +43,97 @@ const swiper = new Swiper('.swiper-form', {
   },
 })
 
-// enter on input to next slide
-const formInputs = document.querySelectorAll('.form-input')
-formInputs.forEach(input => {
-  input.addEventListener('keyup', e => {
-    if(e.key == 'Enter') {
-      swiper.slideNext()
+showHideNavigation()
+
+swiper.on('slideChange', function () {
+  showHideNavigation()
+  // auto focus input after slide change
+  const activeSlide = swiper.slides[swiper.activeIndex]
+  const activeSlideInput = activeSlide?.querySelector('.form-input')
+
+  // if input no have value, disabled navigation
+  if(activeSlideInput) {
+    if(activeSlideInput?.value) {
+      navigationActive(true)
+    } else {
+      navigationActive(false)
+    }
+  } else {
+    navigationActive(true)
+  }
+});
+
+
+// pagination style logic
+if(swiper.activeIndex === 0 ) {
+  paginationForm.style.opacity = '0'
+} else {
+  paginationForm.style.opacity = '100'
+}
+
+swiper.on('paginationUpdate', (swiper) => {
+  const paginations = document.querySelectorAll('.swiper-pagination-form-bullet')
+
+  if(swiper.isBeginning) {
+    paginationForm.style.opacity = '0'
+  } else {
+    paginationForm.style.opacity = '100'
+  }
+  paginations.forEach((page, i) => {
+    if((i - 1) < swiper.activeIndex ) {
+      page.classList.add('swiper-pagination-form-bullet-active')
     }
   })
 })
 
-const navigationForm = document.querySelectorAll('.swiper-navigation-form')
+// onsubmit form
+formSubmitButton.addEventListener('click', () => {
+  thanksForSubmit.classList.remove('!hidden')
+})
+
+// Toggle Menu
+toggleMenu.addEventListener('click', () => {
+  menu.classList.toggle('translate-y-[-150%]')
+  menuIcon.src = menu.classList.contains('translate-y-[-150%]') ? '/assets/images/menu.svg' : '/assets/images/menu-close.svg'
+})
+
+
+let emailErrorText = "there's must be valid email"
+let urlErrorText = "there's must be valid url"
+
+formInputs.forEach(input => {
+  
+
+
+  input.addEventListener('input', e => {
+
+    // check required
+    if(e.target.type == 'email') {
+      const errorEmail = document.querySelector(`.error-${e.target.id}`)
+      errorEmail.textContent = !validateEmail(e.target.value) ? emailErrorText : ""
+      isInputValid(e.target, validateEmail(e.target.value))
+      navigationActive(validateEmail(e.target.value))
+      return
+    }
+    if(e.target.type == 'url') {
+      const errorUrl = document.querySelector(`.error-${e.target.id}`)
+      errorUrl.textContent = !validateUrl(e.target.value) ? urlErrorText : ""
+      isInputValid(e.target, validateUrl(e.target.value))
+      navigationActive(validateUrl(e.target.value))
+      return
+    } 
+
+    isInputValid(e.target, e.target.value)
+    navigationActive(!!e.target.value)
+  })
+})
+
+function navigationActive(status) {
+  slideNextButtons.forEach(nextButton => {
+    nextButton.classList[status ? 'remove' : 'add']('navigation-disabled')
+  })
+  swiper.allowSlideNext = status
+}
 
 function showHideNavigation() {
   if(swiper.activeIndex === 0) {
@@ -49,72 +147,17 @@ function showHideNavigation() {
   }
 }
 
-showHideNavigation()
-
-swiper.on('slideChange', function () {
-  console.log('is change!');
-  showHideNavigation()
-  console.log(swiper);
-  // auto focus input after slide change
-  const activeSlide = swiper.slides[swiper.activeIndex]
-  const activeSlideInput = activeSlide?.querySelector('.form-input')
-  const slideNextButtons = document.querySelectorAll('.swiper-navigation-form.swiper-next-slide')
-
-  // if input no have value, disabled navigation
-  if(activeSlideInput) {
-    if(activeSlideInput?.value) {
-      navigationActive(true)
-    } else {
-      navigationActive(false)
-    }
-    activeSlideInput?.addEventListener('input', (e) => {
-      if(e.target.value) {
-        navigationActive(true)
-      } else {
-        navigationActive(false)
-      }
-    })
-  } else {
-    navigationActive(true)
-  }
-
-  function navigationActive(status) {
-    slideNextButtons.forEach(nextButton => {
-      nextButton.classList[status ? 'remove' : 'add']('navigation-disabled')
-    })
-    swiper.allowSlideNext = status
-  }
-});
-
-
-// pagination style logic
-const paginationForm = document.querySelector('.swiper-pagination-form')
-const paginations = document.querySelectorAll('.swiper-pagination-form-bullet')
-
-if(swiper.activeIndex === 0 ) {
-  paginationForm.style.opacity = '0'
-} else {
-  paginationForm.style.opacity = '100'
+function validateEmail (value) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  return regex.test(value) ? true : false
 }
 
-swiper.on('paginationUpdate', (swiper) => {
-  if(swiper.isBeginning) {
-    paginationForm.style.opacity = '0'
-  } else {
-    paginationForm.style.opacity = '100'
-  }
-  paginations.forEach((page, i) => {
-    if((i - 1) < swiper.activeIndex ) {
-      page.classList.add('swiper-pagination-form-bullet-active')
-    }
-  })
-})
+function isInputValid(el, condition) {
+  el.classList[condition ? 'remove' : 'add']('!border-danger')
+  el.classList[condition ? 'add' : 'remove']('!border-primary')
+}
 
-
-// onsubmit form
-const thanksForSubmit = document.querySelector('#thanks-for-submit')
-const formSubmitButton = document.querySelector('.form-submit-button')
-
-formSubmitButton.addEventListener('click', () => {
-  thanksForSubmit.classList.remove('!hidden')
-})
+function validateUrl(value) {
+  const res = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  return (res !== null)
+};
